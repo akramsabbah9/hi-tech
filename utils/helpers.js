8501b440b13a7c3537a5b772c25a352570b7273d
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-15 17:45:35 -0700

    added comment for helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,4 @@
+/* utils/helpers.js: helper functions for handlebars. */
 const format_date = date => {
     const newDate = new Date(date);
     return `${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()}`;
