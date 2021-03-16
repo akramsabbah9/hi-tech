c44fc711bc8fa597d8bc2a23a70db57a2fa5bebc
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-15 17:02:12 -0700

    added single-post view and comment functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -15,8 +15,12 @@ router.get("/", (req, res) => {
 
 // post a comment
 router.post("/", (req, res) => {
-    // expects { comment_text, user_id, post_id } in req.body
-    Comment.create(req.body)
+    // expects { comment_text, post_id, user_id } in req.body
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
+    })
     .then(commentData => res.json(commentData))
     .catch(err => {
         console.log(err);
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,6 +34,41 @@ router.get("/", (req, res) => {
 });
 
 
+// single-post: display a single post and its comments
+router.get("/post/:id", (req, res) => {
+    Post.findOne({
+        where: { id: req.params.id },
+        include: [
+            {
+                model: Comment,
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(postData => {
+        // if nothing was found, respond with 404
+        if (!postData) {
+            return res.status(404).json({ message: "No post found with this id" });
+        }
+        
+        // make the post plaintext and render it in edit-post
+        const post = postData.get({ plain: true });
+        res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+
 // login: send user to login page.
 router.get("/login", (req, res) => {
     // if already logged in, return to homepage
