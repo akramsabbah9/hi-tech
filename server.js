13ddcd4ed4951ab6d2df1ffbd7c2e9c6c6d61220
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-15 17:28:30 -0700

    added helper to format dates

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 
 /* GLOBALS & MIDDLEWARE */
@@ -21,7 +22,7 @@ const sess = {
 };
 
 const app = express();
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 const PORT = process.env.PORT || 3001;
 
 app.engine("handlebars", hbs.engine);
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+const format_date = date => {
+    const newDate = new Date(date);
+    return `${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()}`;
+};
+
+module.exports = { format_date };
