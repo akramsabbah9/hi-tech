5dd73e29436bf66e9d5bc30e0b5c28f794ed710f
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-15 23:54:08 -0700

    styled login and signup pages

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -25,4 +25,4 @@ const signupFormHandler = async event => {
     }
 };
 
-document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
+document.querySelector("#signup-form").addEventListener("submit", signupFormHandler);
